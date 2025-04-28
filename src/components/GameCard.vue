7f5871f264110110e398697d542d0775edc95974
LindaKat <lindakt16@gmail.com>
2025-04-28 17:48:05 -0500

    adjust back top button position; fix game card status alignment

--- src/components/GameCard.vue
+++ src/components/GameCard.vue
@@ -12,7 +12,7 @@ const props = defineProps(["game"]);
         class="time"
         v-if="game.gameStatus === 'Preview' || game.gameStatus === 'Scheduled'"
       >
-        Start time {{ game.time }}
+        Start time <span>{{ game.time }}</span>
       </p>
       <p class="live" v-if="game.gameStatus === 'Live'">LIVE NOW</p>
       <p class="final" v-if="game.gameStatus === 'Final'">Final</p>
@@ -30,6 +30,7 @@ const props = defineProps(["game"]);
 article {
   display: grid;
   grid-template-columns: repeat(3, 1fr);
+  grid-auto-rows: minmax(2.5rem, auto);
   align-items: center;
   gap: 0.75rem;
   padding: 0 0.5rem;
@@ -48,8 +49,13 @@ article {
   justify-self: center;
 }
 
+.time span {
+  display: block;
+}
+
 .live {
   font-weight: 600;
+  color: var(--red);
 }
 
 .score {
